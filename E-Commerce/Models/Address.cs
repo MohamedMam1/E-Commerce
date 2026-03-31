d5b898f89a63ba9c15007f7e0c8a6bd1d37a5236
MohamedMam1 <ullyan620@gmail.com>
2026-03-31 23:41:37 +0200

    Update Address.cs

--- E-Commerce/Models/Address.cs
+++ E-Commerce/Models/Address.cs
@@ -9,10 +9,10 @@ namespace FinalProject.Models
         public int Id { get; set; }
 
         [Required]
-        public string UserId { get; set; }  // Changed to string since IdentityUser.Id is string
+        public string UserId { get; set; }  
 
         [Required]
-        public string AddressLine { get; set; }  // Renamed to AddressLine to avoid conflict with class name
+        public string AddressLine { get; set; }  
 
         // Navigation
         [ForeignKey("UserId")]
