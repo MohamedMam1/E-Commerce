9576b92186579cb71586714c7e1b9d57f28181b0
Abdelrahman Adam <abdelrahmanadam111Ggmail.com>
2026-04-10 20:30:15 +0200

    Updating AdminDashboard-2

--- E-Commerce/Interfaces/IUserRepository.cs
+++ E-Commerce/Interfaces/IUserRepository.cs
@@ -9,6 +9,8 @@ namespace E_Commerce.Interfaces
         Task UpdateUserAddressesAsync(ApplicationUser User, List<string> UpdatedAddresses, string? NewAddressLine);
         Task<List<UserDashBoardVM>> GetUsersWithRolesAsync();
         Task<bool> IsEmailExists(string email);
+        Task<UserPaginationVM> GetUsersWithRolesAsync(string? searchTerm, string? status, string? role, int pageNumber, int pageSize);
+
 
 
     }
